5dc92c901a6cf91b284afa5f24bba00bd9ffa45c
Jeremy David <jeremy.david@iomedia.ch>
2012-11-09 14:13:26 +0100

    Added standards compliance to the return statements in Sami\Console\Command\Command

--- Sami/Console/Command/Command.php
+++ Sami/Console/Command/Command.php
@@ -197,7 +197,10 @@ abstract class Command extends BaseCommand
 
     public function displayParseSummary()
     {
-        if (count($this->transactions) <= 0) return;
+        if (count($this->transactions) <= 0) {
+            return;
+        }
+        
         $this->output->writeln('');
         $this->output->writeln('<bg=cyan;fg=white> Version </>  <bg=cyan;fg=white> Updated C </>  <bg=cyan;fg=white> Removed C </>');
 
@@ -209,7 +212,10 @@ abstract class Command extends BaseCommand
 
     public function displayRenderSummary()
     {
-        if (count($this->diffs) <= 0) return;
+        if (count($this->diffs) <= 0) {
+            return;
+        }
+        
         $this->output->writeln('<bg=cyan;fg=white> Version </>  <bg=cyan;fg=white> Updated C </>  <bg=cyan;fg=white> Updated N </>  <bg=cyan;fg=white> Removed C </>  <bg=cyan;fg=white> Removed N </>');
 
         foreach ($this->diffs as $version => $diff) {
