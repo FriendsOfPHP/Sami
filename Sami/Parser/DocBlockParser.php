df11b17f5df0593858aada6b3954c6401d4736dc
Kevin Herrera <kherrera@cinahl.com>
2013-05-01 16:11:42 -0700

    Removing single leading space.
    This caused issues with Markdown formatted lists.

--- Sami/Parser/DocBlockParser.php
+++ Sami/Parser/DocBlockParser.php
@@ -73,6 +73,10 @@ class DocBlockParser
                 $long = trim(substr($short, strlen($match[0])));
                 $short = trim($match[0]);
             }
+
+            // remove single lead space
+            $short = preg_replace('/^\s/', '', $short);
+            $long = preg_replace('/^\s/m', '', $long);
         }
 
         $this->position = 'tag';
