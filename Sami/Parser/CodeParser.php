58ea1114dd737cecc2a3f4e357206a062ebdaf18
Fabien Potencier <fabien.potencier@gmail.com>
2013-09-27 19:22:25 +0200

    fixed support for PHPParser 0.9.1 (closes #59)

--- Sami/Parser/CodeParser.php
+++ Sami/Parser/CodeParser.php
@@ -16,12 +16,19 @@ class CodeParser
     protected $parser;
     protected $traverser;
     protected $context;
+    protected $passCode;
 
     public function __construct(ParserContext $context, \PHPParser_Parser $parser, \PHPParser_NodeTraverser $traverser)
     {
         $this->context = $context;
         $this->parser = $parser;
         $this->traverser = $traverser;
+
+        // hack for a BC break between PHPParser 0.9.1 and 0.9.2
+        // the Parser::parse() method argument changed
+        $m = new \ReflectionMethod($this->parser, 'parse');
+        $parameters = $m->getParameters();
+        $this->passCode = null === $parameters[0]->getClass();
     }
 
     public function getContext()
@@ -32,7 +39,11 @@ class CodeParser
     public function parse($code)
     {
         try {
-            $this->traverser->traverse($this->parser->parse($code));
+            if ($this->passCode) {
+                $this->traverser->traverse($this->parser->parse($code));
+            } else {
+                $this->traverser->traverse($this->parser->parse(new \PHPParser_Lexer($code)));
+            }
         } catch (\PHPParser_Error $e) {
             $this->context->addError($this->context->getFile(), 0, $e->getMessage());
         }
