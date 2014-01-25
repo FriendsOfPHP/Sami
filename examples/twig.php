81574456d90f4f6034cc39ca00546106657728ce
Fabien Potencier <fabien.potencier@gmail.com>
2014-01-25 09:17:35 +0100

    added Travis configuration

--- examples/twig.php
+++ examples/twig.php
@@ -1,10 +1,20 @@
 <?php
 
-return new Sami\Sami('/path/to/Twig/lib', array(
+use Sami\Version\GitVersionCollection;
+
+$dir = '/Users/fabien/Code/github/fabpot/Twig/lib';
+
+$versions = GitVersionCollection::create($dir)
+    ->addFromTags('v1.*')
+    ->add('master', 'master branch')
+;
+
+return new Sami\Sami($dir, array(
     'theme'                => 'enhanced',
     'title'                => 'Twig 1.6 API',
-    'build_dir'            => __DIR__.'/../build/twig',
-    'cache_dir'            => __DIR__.'/../cache/twig',
+    'build_dir'            => __DIR__.'/../build/twig/%version%',
+    'cache_dir'            => __DIR__.'/../cache/twig/%version%',
     'simulate_namespaces'  => true,
     'default_opened_level' => 1,
+    'versions' => $versions,
 ));
