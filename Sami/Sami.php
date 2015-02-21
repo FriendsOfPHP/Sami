b850ab439114a845f612543f3936a8604db85956
Fabien Potencier <fabien.potencier@gmail.com>
2015-02-21 13:44:56 +0100

    prepared version 3.0.2

--- Sami/Sami.php
+++ Sami/Sami.php
@@ -34,7 +34,7 @@ use Sami\Version\Version;
 
 class Sami extends Container
 {
-    const VERSION = '3.0.2-DEV';
+    const VERSION = '3.0.2';
 
     public function __construct($iterator = null, array $config = array())
     {
