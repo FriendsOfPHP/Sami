2b3c0a2a0c2b78912e826038bcaae81607f8e24c
Fabien Potencier <fabien.potencier@gmail.com>
2015-02-21 13:45:36 +0100

    bumped version to 3.0.3

--- Sami/Sami.php
+++ Sami/Sami.php
@@ -34,7 +34,7 @@ use Sami\Version\Version;
 
 class Sami extends Container
 {
-    const VERSION = '3.0.2';
+    const VERSION = '3.0.3-DEV';
 
     public function __construct($iterator = null, array $config = array())
     {
