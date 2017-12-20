336f2f35d0570a130bd1bb0f83118308d39e805e
Fabien Potencier <fabien.potencier@gmail.com>
2017-12-20 07:31:56 -0800

    bumped version to 4.0.12-DEV

--- Sami/Sami.php
+++ Sami/Sami.php
@@ -34,7 +34,7 @@ use Sami\Version\Version;
 
 class Sami extends Container
 {
-    const VERSION = '4.0.11';
+    const VERSION = '4.0.12-DEV';
 
     public function __construct($iterator = null, array $config = array())
     {
