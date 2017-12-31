77c441e606849e8384c5552eab43b6bb25baeb2a
Fabien Potencier <fabien.potencier@gmail.com>
2017-12-31 05:08:45 +0100

    prepared RELEASE 4.0.12

--- Sami/Sami.php
+++ Sami/Sami.php
@@ -34,7 +34,7 @@ use Sami\Version\Version;
 
 class Sami extends Container
 {
-    const VERSION = '4.0.12-DEV';
+    const VERSION = '4.0.12';
 
     public function __construct($iterator = null, array $config = array())
     {
